d4b11cc4f0a11589d95985fddab1cca6880e7aac
lais-fernanda <laisfa28@hotmail.com>
2022-05-08 16:49:47 -0300

    warren tech testes

---
+++ teste2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;  
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Vetor com tempo de chegada!
+        int[] tempoChegada = new int[] {-2, -1, 0, 1, 2};
+
+        for (int i = 0; i < 5; i++)
+        {
+            int x = 3;
+            // verifica se tempo de chegada é menor ou igual a 0,
+            // E se o numero de alunos é menor ou igual a 3
+            // se essas condição forem verdadeiras retorna "Aula normal" 
+            if(tempoChegada[0] <= 0 && x <=3)
+            {
+                Console.WriteLine("Aula normal!!: {0}, tempo: {1}", x, tempoChegada[0]);
+            }
+            else
+            {
+                Console.WriteLine("Aula cancelado!!: {0}, tempo: {1}", x, tempoChegada[0]);
+
+            }
+        }
+    }
+}
---
+++ teste3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;  
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Vetor com valeres a serem somados!
+        int[] v = new int[3] {3, 4, 3};
+        int soma = 0;
+        int n = 10;
+
+        // Perconrrendo o vetor
+        for (int i = 0; i <v.Length; i++)
+        {
+            // Fazendo o calculo dos valores.
+            soma += v[i];
+            // Verificando se o calculo atingiu o valor determinado.
+            if (soma <= n){
+                // Exibindo na tela os valores somados, e a soma!
+                    Console.WriteLine(" Vetor: {0} , soma: {1}",  v[i], soma);
+            }
+        }
+    }
+}
